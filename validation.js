8b952a984d67a433002ed68ac95cd85d7b0a1c04
tanmai <tanmayee.pokade@gmail.com>
2020-11-28 09:54:34 +0530

    project

---
+++ validation.js
@@ -0,0 +1,7 @@
+
+function validate(){
+    var fullname = document.getElementById("full-name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message").value;
+}
